79c8f41a6a2c5c3bd5d8e57f4840b58bd06f46a3
robin <robinbiju1901@gmail.com>
2023-01-16 08:54:01 +0530

    this is me

--- routes/users.js
+++ routes/users.js
@@ -224,7 +224,7 @@ router.get("/", async function (req, res, next) {
     ? await cartHelpers.getCartCount(userLog._id)
     : null;
   // let banner = await itemHelpers.getAllBanner();
-  let categories = await productHelpers.getCategories();
+  // let categories = await productHelpers.getCategories();
   let products = await productHelpers.getHomeProducts();
 
   if (userLog) {
@@ -246,7 +246,6 @@ router.get("/", async function (req, res, next) {
     userLog,
     products,
     cartCount,
-    categories,
   });
 });
 
